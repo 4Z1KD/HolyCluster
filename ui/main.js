c202c09fa8302a4a23014f136fb4dd113fc63cec
Omer Sarig <omersarig@hiveware.io>
2024-06-14 22:51:30 +0300

    Refactor the code into class for clarity and order

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,123 @@
-let geojson
+class HolyMap {
+    static projection_types = [
+        "AzimuthalEqualArea",
+        "AzimuthalEquidistant",
+        "Orthographic",
+        "Equirectangular",
+    ]
 
-const projection_types = [
-  "AzimuthalEqualArea",
-  "AzimuthalEquidistant",
-  "Orthographic",
-  "Equirectangular",
-]
+    constructor(map_data, lines) {
+        // This fixes the order of polygon points for d3 compatability
+        this.geojson = rewind(map_data, true)
+        this.lines = lines
+        this.projection = d3.geoAzimuthalEquidistant()
+        this.geo_generator = d3.geoPath().projection(this.projection)
+        this.graticule = d3.geoGraticule()
+        this.state = {
+            type: "AzimuthalEqualArea",
+            scale: 120,
+            q0: null,
+            v0: null,
+            r0: null
+        }
+    }
+
+    attach() {
+        this.init_menu()
+        const drag = d3
+            .drag()
+            .on("start", event => this.drag_started(event))
+            .on("drag", event => this.dragged(event))
+
+        d3.select("svg").call(drag)
+        this.update()
+    }
+
+    init_menu() {
+        d3.select("#menu")
+            .selectAll(".slider.item input")
+            .on("input", element => {
+                this.state.scale = element.target.value
+                this.update()
+            })
+
+        d3.select("#menu .projection-type select")
+            .on("change", event => {
+                this.state.type = event.target.options[event.target.selectedIndex].value
+                this.projection = d3["geo" + this.state.type]()
+                this.geo_generator.projection(this.projection)
+                this.update()
+            })
+            .selectAll("#menu .projection-type select option")
+            .data(HolyMap.projection_types)
+            .enter()
+            .append("option")
+            .attr("value", function (d) { return d })
+            .text(function (d) { return d })
+
+        d3.select("#menu .reset")
+            .on("click", _ => {
+                this.state.scale = 120
+                this.projection.rotate([0, 0, 0])
+                this.update()
+            })
+            .selectAll("input[type=range]")
+            .data(this.state)
+            .attr("value", function(d) { return d })
+    }
+
+    update() {
+        // Update projection
+        this.projection
+            .scale(this.state.scale)
+            .translate([600, 300])
+
+        // Update world map
+        let u = d3.select("g.map")
+            .selectAll("path")
+            .data(this.geojson.features)
+
+        u.enter()
+            .append("path")
+            .merge(u)
+            .attr("d", this.geo_generator)
+
+        u = d3.select("g.lines")
+            .selectAll("path")
+            .data(this.lines)
+
+        u.enter()
+            .append("path")
+            .merge(u)
+            .attr("d", this.geo_generator)
+
+        // Update projection center
+        const projectedCenter = this.projection([0, 0])
+        d3.select(".projection-center")
+            .attr("cx", projectedCenter[0])
+            .attr("cy", projectedCenter[1])
+
+        // Update graticule
+        d3.select(".graticule path")
+            .datum(this.graticule())
+            .attr("d", this.geo_generator)
+    }
+
+    drag_started(event) {
+        this.state.v0 = versor.cartesian(this.projection.invert([event.x, event.y]))
+        this.state.r0 = this.projection.rotate()
+        this.state.q0 = versor(this.state.r0)
+    }
+
+    dragged(event) {
+        const coordinates = this.projection.rotate(this.state.r0).invert([event.x, event.y])
+        const v1 = versor.cartesian(coordinates)
+        const q1 = versor.multiply(this.state.q0, versor.delta(this.state.v0, v1))
+        const r1 = versor.rotation(q1)
+        this.projection.rotate(r1)
+        this.update()
+    }
+}
 
 const lines = [
     {type: "LineString", coordinates: [[0.1278, 51.5074], [-74.0059, 40.7128]]},
@@ -13,116 +125,9 @@ const lines = [
     {type: "LineString", coordinates: [[34.851612, 31.046051],[144.948693002555, -37.854759842877925] ]}
 ]
 
-let projection = d3.geoAzimuthalEquidistant()
-const geo_generator = d3.geoPath().projection(projection)
-
-const graticule = d3.geoGraticule()
-
-const state = {
-    type: "AzimuthalEqualArea",
-    scale: 120,
-}
-
-function init_menu() {
-    d3.select("#menu")
-        .selectAll(".slider.item input")
-        .on("input", function (_) {
-            const attr = d3.select(this).attr("name")
-            state[attr] = this.value
-            d3.select(this.parentNode.parentNode).select(".value").text(this.value)
-            update()
-        })
-
-    d3.select("#menu .projection-type select")
-        .on("change", function (_) {
-            state.type = this.options[this.selectedIndex].value
-            projection = d3["geo" + state.type]()
-            geo_generator.projection(projection)
-            update()
-        })
-        .selectAll("#menu .projection-type select option")
-        .data(projection_types)
-        .enter()
-        .append("option")
-        .attr("value", function (d) { return d })
-        .text(function (d) { return d })
-
-    d3.select("#menu .reset")
-        .on("click", function (_) {
-            state.scale = 120
-            projection.rotate([0, 0, 0])
-            update()
-        })
-        .selectAll("input[type=range]")
-        .data(state)
-        .attr("value", function(d) { constole.log("aaaa"); return d })
-}
-
-function update() {
-    // Update projection
-    projection
-        .scale(state.scale)
-        .translate([600, 300])
-
-    // Update world map
-    let u = d3.select("g.map")
-        .selectAll("path")
-        .data(geojson.features)
-
-    u.enter()
-        .append("path")
-        .merge(u)
-        .attr("d", geo_generator)
-
-    u = d3.select("g.lines")
-        .selectAll("path")
-        .data(lines)
-
-    u.enter()
-        .append("path")
-        .merge(u)
-        .attr("d", geo_generator)
-
-    // Update projection center
-    const projectedCenter = projection([0, 0])
-    d3.select(".projection-center")
-        .attr("cx", projectedCenter[0])
-        .attr("cy", projectedCenter[1])
-
-    // Update graticule
-    d3.select(".graticule path")
-        .datum(graticule())
-        .attr("d", geo_generator)
-}
-
-let q0, v0, r0
-function drag_started(event) {
-    v0 = versor.cartesian(projection.invert([event.x, event.y]))
-    r0 = projection.rotate()
-    q0 = versor(r0)
-}
-
-function dragged(event) {
-    const v1 = versor.cartesian(projection.rotate(r0).invert([event.x, event.y]))
-    const q1 = versor.multiply(q0, versor.delta(v0, v1))
-    const r1 = versor.rotation(q1)
-    projection.rotate(r1)
-    update()
-}
-
-const drag = d3
-    .drag()
-    .on("start", drag_started)
-    .on("drag", dragged)
-
-d3.select("svg").call(drag)
-
-
 // The agalega and st brandon dxcc is a multi polygon that is made of 2 ring,
 // That I switched in order manually. This is most likly a bug in the rewind function.
-const geojson_url = "./dxcc.geojson"
-d3.json(geojson_url).then(data => {
-    geojson = rewind(data, true)
-    init_menu()
-    update()
+d3.json("./dxcc.geojson").then(data => {
+    const holy_map = new HolyMap(data, lines)
+    holy_map.attach()
 })
