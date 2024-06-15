60d95db12ccf589db06e74029d4adc65d0cb56ab
yoav <yoav.katzman@campus.technion.ac.il>
2024-06-15 11:34:34 +0300

    dx sql model

--- src/api/main.py
+++ src/api/main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
 from sqlmodel import Field, SQLModel
 import uvicorn
+import datetime
+
+class DX(SQLModel, table=True):
+    number: int
+    spotter: str
+    frequency: str
+    dx_call: str
+    time: datetime.time
+    date: datetime.date
+    beacon: bool
+    mm: bool
+    am: bool
+    am: bool
+    valid: bool
+    lotw: bool
+    lotw_date: bool
+    esql: bool
+    dx_homecall: str
+    comment: str
+    flag: str
+    band: str
+    continent_dx: str
+    continent_spotter: str
+    dx_locator: str
 
 
 app = FastAPI()
