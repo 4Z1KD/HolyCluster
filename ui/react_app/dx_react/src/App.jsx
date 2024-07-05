d17345286cbea5093028425a8c0fa62caa1ace97
Omer Sarig <omersarig@hiveware.io>
2024-07-05 23:40:28 +0300

    Add basic layout of the cluster

--- ui/react_app/dx_react/src/App.jsx
+++ ui/react_app/dx_react/src/App.jsx
@@ -1,5 +1,26 @@
 function App() {
-    return <div>App</div>;
+    const main_squares_classes = [
+        "w-full",
+        "flex-auto",
+        "p-5",
+        "aspect-square",
+        "text-center",
+    ];
+
+    return (
+        <>
+        <h1 className="pb-10 container mx-auto text-2xl font-bold text-slate-600">The holly cluster!</h1>
+        <div className="mx-20 shadow-xl rounded-2xl border-solid border-slate-200 border-2">
+            <div className="p-5 w-full mx-auto rounded-t-2xl border-b-solid border-b-sky border-b-2">
+            Band list
+            </div>
+            <div className="flex divide-x divide-slate-300">
+                <div className={main_squares_classes.join(" ")}>Map</div>
+                <div className={main_squares_classes.join(" ")}>Spots</div>
+            </div>
+        </div>
+        </>
+    );
 }
 
 export default App;
