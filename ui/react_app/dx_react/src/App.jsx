ee80f152c3c1f3d7e85e9e11e16f3b1df4af9c39
Omer Sarig <omersarig@hiveware.io>
2024-07-06 01:39:26 +0300

    Start rebuilding the UI in the react app

--- ui/react_app/dx_react/src/App.jsx
+++ ui/react_app/dx_react/src/App.jsx
@@ -5,7 +5,15 @@ function App() {
         "p-5",
         "aspect-square",
         "text-center",
-    ];
+    ].join(" ");
+
+    const projection_types = [
+        "AzimuthalEquidistant",
+        "AzimuthalEqualArea",
+        "Orthographic",
+        // Equirectangular projection is currently broken due to zoom and drag bhaviours
+        // "Equirectangular",
+    ]
 
     return (
         <>
@@ -15,8 +23,21 @@ function App() {
             Band list
             </div>
             <div className="flex divide-x divide-slate-300">
-                <div className={main_squares_classes.join(" ")}>Map</div>
-                <div className={main_squares_classes.join(" ")}>Spots</div>
+                <div className={main_squares_classes}>
+                    <svg className="aspect-square w-full">
+                        {/* Just a shape for example */}
+                        <circle cx="300" cy="300" r="100"></circle>
+                    </svg>
+                    <div className="relative bottom-0 p-3 space-x-4">
+                        <select className="p-2 rounded-lg">
+                            {projection_types.map(type => <option value={type}>{type}</option>)}
+                        </select>
+                        <button className="text-white bg-blue-600 active:bg-blue-800 hover:bg-blue-700 font-medium rounded-lg text-sm px-4 py-2">Reset</button>
+                        <input type="checkbox" autocomplete="off"></input>
+                        <label className="text-slate-700">Show night</label>
+                    </div>
+                </div>
+                <div className={main_squares_classes}>Spots</div>
             </div>
         </div>
         </>
