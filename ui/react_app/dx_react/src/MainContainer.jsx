e8fa94c73dca0b089a3dfccc201b7e095210189c
Omer Sarig <omersarig@hiveware.io>
2024-07-06 12:15:49 +0300

    Add selecting projection type

--- ui/react_app/dx_react/src/MainContainer.jsx
+++ ui/react_app/dx_react/src/MainContainer.jsx
@@ -1,5 +1,6 @@
 import Map from "./Map.jsx";
 import MapControls from "./MapControls.jsx";
+import { useState } from "react";
 
 function MainContainer() {
     const main_squares_classes = [
@@ -10,6 +11,8 @@ function MainContainer() {
         "text-center",
     ].join(" ");
 
+    const [projection_type, set_projection_type] = useState("AzimuthalEquidistant");
+
     return (
         <div className="mx-20 shadow-xl rounded-2xl border-solid border-slate-200 border-2">
             <div className="p-5 w-full mx-auto rounded-t-2xl border-b-solid border-b-sky border-b-2">
@@ -17,8 +20,8 @@ function MainContainer() {
             </div>
             <div className="flex divide-x divide-slate-300">
                 <div className={main_squares_classes}>
-                    <Map/>
-                    <MapControls/>
+                    <Map projection_type={projection_type}/>
+                    <MapControls set_projection_type={set_projection_type}/>
                 </div>
                 <div className={main_squares_classes}>Spots</div>
             </div>
--- ui/react_app/dx_react/src/MapControls.jsx
+++ ui/react_app/dx_react/src/MapControls.jsx
@@ -1,15 +1,17 @@
-function MapControls() {
+function MapControls({ set_projection_type }) {
     const projection_types = [
         "AzimuthalEquidistant",
         "AzimuthalEqualArea",
         "Orthographic",
-        // Equirectangular projection is currently broken due to zoom and drag bhaviours
-        // "Equirectangular",
     ]
+
+    function on_projection_change(event) {
+        set_projection_type(event.target.value);
+    }
 
     return (
         <div className="relative bottom-0 p-3 space-x-4">
-            <select className="rounded-lg px-4 py-2">
+            <select className="rounded-lg px-4 py-2" onChange={on_projection_change}>
                 {projection_types.map(type => <option key={type} value={type}>{type}</option>)}
             </select>
             <button className="text-white bg-blue-600 active:bg-blue-800 hover:bg-blue-700 font-medium rounded-lg text-sm px-4 py-2">
