cf20fa566c1bdaab65624bde608b4b25d3aeff1b
Omer Sarig <omersarig@hiveware.io>
2024-07-06 11:58:13 +0300

    Draw the map

--- ui/react_app/dx_react/src/Map.jsx
+++ ui/react_app/dx_react/src/Map.jsx
@@ -1,13 +1,17 @@
 import * as d3 from "d3";
 import geojsonRewind from "@mapbox/geojson-rewind";
-import dxcc_map from "./dxcc_map.json";
+import dxcc_map_raw from "./dxcc_map.json";
+
+const dxcc_map = geojsonRewind(dxcc_map_raw, true);
 
 function Map({
     width = 700,
     height = 700,
     projection_type = "AzimuthalEquidistant",
 }) {
-    const projection = d3["geo" + projection_type]().fitSize([width, height], dxcc_map);
+    const projection = d3["geo" + projection_type]()
+        .precision(0.1)
+        .fitSize([width, height], dxcc_map);
     const path_generator = d3.geoPath().projection(projection);
     const graticule = d3.geoGraticule10();
 
@@ -32,7 +36,13 @@ function Map({
                     d={path_generator(graticule)}
                 ></path>
             </g>
-            <g className="map"></g>
+            <g className="map">
+                {
+                    dxcc_map.features.map(shape => {
+                        return <path fill="#def7cf" stroke="#777" d={path_generator(shape)}/>
+                    })
+                }
+            </g>
             <g className="lines"></g>
         </g>
     </svg>;
