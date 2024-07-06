4726b89e3fba29ab045b1623db0eb3f4aff0e7b8
Omer Sarig <omersarig@hiveware.io>
2024-07-06 11:51:09 +0300

    Draw basic graticule

--- ui/react_app/dx_react/src/Map.jsx
+++ ui/react_app/dx_react/src/Map.jsx
@@ -1,7 +1,16 @@
+import * as d3 from "d3";
+import geojsonRewind from "@mapbox/geojson-rewind";
 import dxcc_map from "./dxcc_map.json";
 
-function Map({ width = 700, height = 700 }) {
-    console.log(dxcc_map);
+function Map({
+    width = 700,
+    height = 700,
+    projection_type = "AzimuthalEquidistant",
+}) {
+    const projection = d3["geo" + projection_type]().fitSize([width, height], dxcc_map);
+    const path_generator = d3.geoPath().projection(projection);
+    const graticule = d3.geoGraticule10();
+
     return <svg className="aspect-square w-full" width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
         <defs>
             <clipPath id="map-clip">
@@ -14,9 +23,14 @@ function Map({ width = 700, height = 700 }) {
             cy={height / 2}
             style={{fill: "none", stroke: "black"}}
         />
-        <g clip-path="url(#map-clip)">
+
+        <g clipPath="url(#map-clip)">
             <g className="graticule">
-                <path></path>
+                <path
+                    fill="none"
+                    stroke="#eee"
+                    d={path_generator(graticule)}
+                ></path>
             </g>
             <g className="map"></g>
             <g className="lines"></g>
--- ui/react_app/dx_react/src/MapControls.jsx
+++ ui/react_app/dx_react/src/MapControls.jsx
@@ -10,12 +10,12 @@ function MapControls() {
     return (
         <div className="relative bottom-0 p-3 space-x-4">
             <select className="rounded-lg px-4 py-2">
-                {projection_types.map(type => <option value={type}>{type}</option>)}
+                {projection_types.map(type => <option key={type} value={type}>{type}</option>)}
             </select>
             <button className="text-white bg-blue-600 active:bg-blue-800 hover:bg-blue-700 font-medium rounded-lg text-sm px-4 py-2">
                 Reset
             </button>
-            <input type="checkbox" autocomplete="off"></input>
+            <input type="checkbox" autoComplete="off"></input>
             <label className="text-slate-700">Show night</label>
         </div>
     );
