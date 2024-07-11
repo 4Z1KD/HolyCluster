13b374a9f4cce57e78d3ce82ea9fda4593fbf508
Omer Sarig <omersarig@hiveware.io>
2024-07-12 00:16:10 +0300

    Use logging instead of prints

--- src/RadioController.py
+++ src/RadioController.py
@@ -1,17 +1,20 @@
 import logging
 
 
+logger = logging.getLogger(__name__)
+
+
 class DummyRadioController:
     def init_radio(self):
         self.rig_type = "dummy"
         self.status_string = "I am a dummy"
-        print("Initializing radio")
+        logger.info("Initializing radio")
 
     def set_mode(self, mode):
-        print(f"Setting mode: {mode}")
+        logger.info(f"Setting mode: {mode}")
 
     def set_frequency(self, slot, freq):
-        print(f"Setting frequency: {freq} in slot {slot}")
+        logger.info(f"Setting frequency: {freq} in slot {slot}")
 
 
 class OmnirigRadioController:
@@ -22,16 +25,14 @@ class OmnirigRadioController:
         self.log_level = log_level
 
     def init_radio(self):
-        '''initialize an omnipyrig instance and set active rig'''
+        """initialize an omnipyrig instance and set active rig"""
         import omnipyrig
         self.omni_client = omnipyrig.OmniRigWrapper()
         # set the active rig to 1 (as defined in OmniRig GUI)
         self.omni_client.setActiveRig(1)
         self.rig_type = self.omni_client.getParam("RigType")
         self.status_string = self.omni_client.getParam("StatusStr")
-        if self.log_level <= logging.INFO:
-            print(f"RigType: {self.rig_type}")
-            print(f"StatusStr: {self.status_string}")
+        logger.debug(f"Rig type: {self.rig_type}, Status string: {self.status_string}")
 
     def set_mode(self, mode):
         mode_to_number = {
