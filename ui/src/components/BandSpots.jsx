005210dc66602e8e82a4374baac55564fc06a579
Omer Sarig <omersarig@hiveware.io>
2024-09-03 01:06:06 +0300

    Make the band spots table header sticky

--- ui/src/components/BandSpots.jsx
+++ ui/src/components/BandSpots.jsx
@@ -14,7 +14,7 @@ function BandSpots({
         <div className="basis-[49%] border-slate-400 border-solid border-2 rounded-2xl p-0 max-h-80 overflow-y-auto min-w-80 max-w-[50%]">
             <table className="table-fixed w-full">
                 <tbody className="divide-y divide-slate-200">
-                    <tr style={{backgroundColor: color}}>
+                    <tr className="sticky top-0" style={{backgroundColor: color}}>
                         <td>Time</td>
                         <td>DX</td>
                         <td><strong>{band}m</strong></td>
