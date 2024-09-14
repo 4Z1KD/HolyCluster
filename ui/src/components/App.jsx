a83edff5255d79e1e9afba5f0cfbe5f52ee14680
Omer Sarig <omersarig@hiveware.io>
2024-09-14 13:01:02 +0300

    Remove unnecessary App component

--- ui/src/components/App.jsx
+++
@@ -1,7 +0,0 @@
-import MainContainer from "./MainContainer.jsx";
-
-function App() {
-    return <MainContainer/>;
-}
-
-export default App;
--- ui/src/components/main.jsx
+++ ui/src/components/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import MainContainer from './MainContainer.jsx'
 import '../index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MainContainer/>
   </React.StrictMode>,
 )
