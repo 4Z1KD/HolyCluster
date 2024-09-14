50c26f3757300086372a72d785078c0f15134376
Omer Sarig <omersarig@hiveware.io>
2024-09-14 13:18:45 +0300

    Move main js file out of components dir

---
+++ ui/src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import MainContainer from "@/components/MainContainer.jsx"
+import "@/index.css"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MainContainer/>
+  </React.StrictMode>,
+)
