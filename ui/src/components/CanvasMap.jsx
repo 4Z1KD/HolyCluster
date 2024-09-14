20834d0b000cc6a318c53f96bb2ac09982c43d9d
Omer Sarig <omersarig@hiveware.io>
2024-09-14 14:44:28 +0300

    Implement circle outline rendering with clipping

--- ui/src/components/CanvasMap.jsx
+++ ui/src/components/CanvasMap.jsx
@@ -22,6 +22,7 @@ function CanvasMap({
     const inner_padding = 50;
     const center_x = dimensions.width / 2;
     const center_y = dimensions.height / 2;
+    const radius = Math.min(center_x, center_y) - inner_padding;
     const [center_lon, center_lat] = map_controls.location.location;
 
     const projection = d3["geo" + map_controls.projection_type]()
@@ -37,16 +38,31 @@ function CanvasMap({
         const canvas = canvas_ref.current;
         const context = canvas.getContext("2d");
 
+        // Clear the map before rendering
+        context.clearRect(0, 0, dimensions.width, dimensions.height);
+
         const path_generator = d3
             .geoPath()
             .projection(projection)
             .context(context);
 
+        // Map outline
+        context.beginPath();
+        context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+        context.stroke();
+
+        // Clip the map content to the circle
+        context.beginPath();
+        context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+        context.clip();
+
+        // Render the graticule
         context.beginPath();
         path_generator(d3.geoGraticule10())
         context.strokeStyle = "#eeeeee";
         context.stroke();
 
+        // Render the map countries from geojson
         dxcc_map.features.forEach(feature => {
             context.beginPath();
             path_generator(feature);
@@ -56,7 +72,7 @@ function CanvasMap({
             context.stroke();
         });
 
-    }, [dxcc_map]);
+    }, [dxcc_map, center_lon, center_lat]);
 
     return <canvas
         ref={canvas_ref}
