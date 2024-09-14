8c6517bd58259927abd905b8e8cf4a19ae64bd11
Omer Sarig <omersarig@hiveware.io>
2024-09-14 16:04:17 +0300

    Implement zoom behavior

--- ui/src/components/CanvasMap.jsx
+++ ui/src/components/CanvasMap.jsx
@@ -38,47 +38,81 @@ function CanvasMap({
         const canvas = canvas_ref.current;
         const context = canvas.getContext("2d");
 
-        // Clear the map before rendering
-        context.clearRect(0, 0, dimensions.width, dimensions.height);
-
         const path_generator = d3
             .geoPath()
             .projection(projection)
             .context(context);
 
-        // Map outline
-        context.beginPath();
-        context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-        context.stroke();
+        function draw_map(transform) {
+            // Clear the map before rendering
+            context.clearRect(0, 0, dimensions.width, dimensions.height);
 
-        // Clip the map content to the circle
-        context.beginPath();
-        context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
-        context.clip();
+            context.save()
 
-        // Render the graticule
-        context.beginPath();
-        path_generator(d3.geoGraticule10())
-        context.strokeStyle = "#eeeeee";
-        context.stroke();
-
-        // Render the map countries from geojson
-        dxcc_map.features.forEach(feature => {
+            // Map outline
             context.beginPath();
-            path_generator(feature);
-            context.fillStyle = "#def7cf";
-            context.strokeStyle = "#777777";
-            context.fill();
+            context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
             context.stroke();
-        });
 
+            // Clip the map content to the circle
+            context.beginPath();
+            context.arc(center_x, center_y, radius, 0, 2 * Math.PI);
+            context.clip();
+
+            if (transform != null) {
+                context.setTransform(
+                    transform.k, 0, 0,
+                    transform.k, transform.x, transform.y,
+                    1, 1, 1
+                );
+                context.lineWidth = 1 / transform.k;
+            }
+
+            // Render the graticule
+            context.beginPath();
+            path_generator(d3.geoGraticule10())
+            context.strokeStyle = "#eeeeee";
+            context.stroke();
+
+            // Render the map countries from geojson
+            dxcc_map.features.forEach(feature => {
+                context.beginPath();
+                path_generator(feature);
+                context.fillStyle = "#def7cf";
+                context.strokeStyle = "#777777";
+                context.fill();
+                context.stroke();
+            });
+
+            context.restore()
+        }
+
+        draw_map();
+
+        let is_drawing = false;
+        const zoom = d3.zoom()
+            .scaleExtent([1, 20])
+            .translateExtent([[0, 0], [dimensions.width, dimensions.height]])
+            .on("zoom", event => {
+                const { transform } = event;
+                if (!is_drawing) {
+                    is_drawing = true;
+                    requestAnimationFrame(() => {
+                        context.clearRect(0, 0, dimensions.width, dimensions.height);
+                        draw_map(transform);
+                        is_drawing = false;
+                    })
+                }
+            }
+        );
+        d3.select(canvas).call(zoom);
     }, [dxcc_map, center_lon, center_lat]);
 
     return <canvas
         ref={canvas_ref}
         width={dimensions.width}
         height={dimensions.height}
-    ></canvas>;
+    />;
 }
 
 export default CanvasMap;
