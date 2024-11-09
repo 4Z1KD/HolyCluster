39c45ffa56fb28b9ea75c074f1129bf1721c5302
Omer Sarig <omersarig@hiveware.io>
2024-11-09 12:46:00 +0200

    Add key to continent buttons

--- ui/src/components/Continents.jsx
+++ ui/src/components/Continents.jsx
@@ -4,9 +4,9 @@ import { continents } from "@/filters_data.js";
 function Continents({}) {
     return <div className="w-32 min-w-20 p-2 flex flex-col text-center h-full gap-3 overflow-y-auto">
         <strong>DX</strong>
-        {continents.map(continent => <FilterButton text={continent} is_active={true} on_click={_ => {}}/>)}
+        {continents.map(continent => <FilterButton key={continent} text={continent} is_active={true} on_click={_ => {}}/>)}
         <strong>DE</strong>
-        {continents.map(continent => <FilterButton text={continent} is_active={true} on_click={_ => {}}/>)}
+        {continents.map(continent => <FilterButton key={continent} text={continent} is_active={true} on_click={_ => {}}/>)}
     </div>;
 }
 
