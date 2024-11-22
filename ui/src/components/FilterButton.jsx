cd6a263b8b7ed7bfceb96af013fdc694771340d0
Omer Sarig <omersarig@hiveware.io>
2024-11-22 20:33:44 +0200

    Change filter buttons style

--- ui/src/components/FilterButton.jsx
+++ ui/src/components/FilterButton.jsx
@@ -6,7 +6,7 @@ function FilterButton({
     hover_brightness = "110",
     size = "normal",
 }) {
-    const inactive_background_color = "#AAAAAA";
+    const inactive_background_color = "#FFFFFF";
     const active_text_color = "#000000";
     const inactive_text_color = "#666666";
 
@@ -16,7 +16,6 @@ function FilterButton({
         "text-base",
         "font-bold",
         "rounded-lg",
-        "border-slate-400",
         "border-2",
         "cursor-pointer",
         `hover:brightness-${hover_brightness}`,
@@ -25,6 +24,12 @@ function FilterButton({
         box_style.push(...["py-2", "px-1", "min-w-12"])
     } else if (size == "small") {
         box_style.push(...["w-12"])
+    }
+
+    if (is_active) {
+        box_style.push("border-slate-400");
+    } else {
+        box_style.push("border-slate-700");
     }
 
     return <div
