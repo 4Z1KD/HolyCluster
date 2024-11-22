50b076a570516f62dd8f999b628eb15b9fd3f175
Omer Sarig <omersarig@hiveware.io>
2024-11-22 20:34:01 +0200

    Fix key error in continents column

--- ui/src/components/Continents.jsx
+++ ui/src/components/Continents.jsx
@@ -2,33 +2,40 @@ import FilterOptions from "@/components/FilterOptions.jsx";
 import FilterButton from "@/components/FilterButton.jsx";
 import { continents } from "@/filters_data.js";
 
-function Continents({ filters, set_filters }) {
-    const title = { dx: "DX", spotter: "DE" };
+const title = { dx: "DX", spotter: "DE" };
 
+function ContinentColumn({ spot_type, filters, set_filters }) {
+    const filter_key = `${spot_type}_continents`;
+
+    return <>
+        <strong>{title[spot_type]}</strong>
+        {continents.map(continent => <FilterOptions
+            key={spot_type + "_" + continent}
+            set_filters={set_filters}
+            filter_key={filter_key}
+            filter_value={continent}
+            orientation="left"
+            align="center">
+                <FilterButton
+                    text={continent}
+                    is_active={filters[filter_key][continent]}
+                    on_click={_ => {
+                        set_filters(state => state[filter_key][continent] = !state[filter_key][continent]);
+                    }}
+                    size="small"/>
+            </FilterOptions>
+        )}
+    </>;
+}
+
+function Continents({ filters, set_filters }) {
     return <div className="w-32 p-2 flex flex-col text-center h-full gap-3">
-        {["dx", "spotter"].map(spot_type => {
-            const filter_key = `${spot_type}_continents`;
-            return <>
-                <strong>{title[spot_type]}</strong>
-                {continents.map(continent => <FilterOptions
-                    key={spot_type + "_" + continent}
-                    set_filters={set_filters}
-                    filter_key={filter_key}
-                    filter_value={continent}
-                    orientation="left"
-                    align="center">
-                        <FilterButton
-                            key={spot_type + "_" + continent}
-                            text={continent}
-                            is_active={filters[filter_key][continent]}
-                            on_click={_ => {
-                                set_filters(state => state[filter_key][continent] = !state[filter_key][continent]);
-                            }}
-                            size="small"/>
-                    </FilterOptions>
-                )}
-            </>;
-        })}
+        {["dx", "spotter"].map(spot_type => <ContinentColumn
+            key={spot_type}
+            spot_type={spot_type}
+            filters={filters}
+            set_filters={set_filters}
+        />)}
     </div>;
 }
 
