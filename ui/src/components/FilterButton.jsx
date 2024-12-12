edaa33b19f0941504c0f361504d214273563072b
Omer Sarig <omersarig@hiveware.io>
2024-12-13 00:43:41 +0200

    Style fixes

--- ui/src/components/FilterButton.jsx
+++ ui/src/components/FilterButton.jsx
@@ -9,16 +9,13 @@ function FilterButton({
     hover_brightness = "110",
     size = "normal",
 }) {
-    const inactive_background_color = "#FFFFFF";
-    const active_text_color = "#000000";
-    const inactive_text_color = "#666666";
-
     const box_style = [
         "max-w-18",
         "text-center",
         "text-base",
         "rounded-full",
         "cursor-pointer",
+        "select-none",
         `hover:brightness-${hover_brightness}`,
     ];
     if (size == "normal") {
@@ -28,18 +25,28 @@ function FilterButton({
     }
 
     if (is_active) {
-        box_style.push("border-slate-700");
+        box_style.push(
+            "border-slate-700",
+            `bg-[${color}]`,
+            `text-[${text_color}]`,
+        );
     } else {
-        box_style.push("border-slate-400");
+        box_style.push(
+            "border-slate-400",
+            "bg-transparent",
+            "outline",
+            "outline-[3px]",
+            "outline-offset-[-3px]",
+            "outline-[#484848]",
+        );
     }
 
     return <div
         className={box_style.join(" ")}
         onClick={on_click}
         style={{
-            backgroundColor: is_active ? color : inactive_background_color,
-            color: text_color,
-            userSelect: "none",
+            backgroundColor: is_active ? color : "transparent",
+            color: is_active ? text_color : "#484848",
         }}>
         <span className="inline-flex items-center space-x-2">
             {text}
