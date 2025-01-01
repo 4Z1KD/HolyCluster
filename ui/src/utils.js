88cb8a91d047a31eb941c64760f4a95644a3de8d
Omer Sarig <omersarig@hiveware.io>
2025-01-01 21:58:49 +0200

    Update the local storage helper to deep merge value

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -17,21 +17,37 @@ export function is_matching_list(list, callsign) {
     });
 }
 
+function is_object(item) {
+    return item && typeof item === "object" && !Array.isArray(item);
+}
+
+function deep_merge(target, source) {
+    let output = Object.assign({}, target);
+    let new_keys_added;
+    if (is_object(target) && is_object(source)) {
+        new_keys_added =
+            new Set(Object.keys(target)).difference(new Set(Object.keys(source))).size != 0;
+
+        Object.keys(source).forEach(key => {
+            if (is_object(source[key])) {
+                if (!(key in target)) {
+                    Object.assign(output, { [key]: source[key] });
+                } else {
+                    const [merged_object, merged_keys_added] = deep_merge(target[key], source[key]);
+                    output[key] = merged_object;
+                    new_keys_added = new_keys_added || merged_keys_added;
+                }
+            } else {
+                Object.assign(output, { [key]: source[key] });
+            }
+        });
+    }
+    return [output, new_keys_added];
+}
+
 export function use_object_local_storage(key, default_value) {
     const [current_value, set_value] = useLocalStorage(key, default_value);
-
-    const should_update = Object.keys(default_value) != Object.keys(current_value);
-
-    let merged_value;
-    if (should_update) {
-        merged_value = Object.fromEntries(
-            Object.entries(default_value).map(([key, default_value]) => {
-                return [key, current_value[key] != null ? current_value[key] : default_value];
-            }),
-        );
-    } else {
-        merged_value = current_value;
-    }
+    const [merged_value, should_update] = deep_merge(default_value, current_value);
 
     useEffect(() => {
         if (should_update) {
