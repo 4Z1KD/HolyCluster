15120e367f0ddd88ba457dc25bce0121ca757000
Omer Sarig <omersarig@hiveware.io>
2025-01-11 23:27:33 +0200

    Change the submit spot icon

--- ui/src/components/SubmitSpot.jsx
+++ ui/src/components/SubmitSpot.jsx
@@ -9,9 +9,14 @@ import { useColors } from "@/hooks/useColors";
 function SubmitIcon({ size }) {
     const { colors } = useColors();
     return (
-        <svg fill={colors.buttons.utility} width={size} height={size} viewBox="0 0 32 32">
-            <title>Submit new spot</title>
-            <path d="M24 12h-16v-4h16v4zM16.001 16.52l-8.485 8.485 2.828 2.828 5.657-5.657 5.657 5.657 2.828-2.828-8.485-8.485z" />
+        <svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+            <path
+                d="M9 12H15M12 9V15M3 12C3 13.1819 3.23279 14.3522 3.68508 15.4442C4.13738 16.5361 4.80031 17.5282 5.63604 18.364C6.47177 19.1997 7.46392 19.8626 8.55585 20.3149C9.64778 20.7672 10.8181 21 12 21C13.1819 21 14.3522 20.7672 15.4442 20.3149C16.5361 19.8626 17.5282 19.1997 18.364 18.364C19.1997 17.5282 19.8626 16.5361 20.3149 15.4442C20.7672 14.3522 21 13.1819 21 12C21 9.61305 20.0518 7.32387 18.364 5.63604C16.6761 3.94821 14.3869 3 12 3C9.61305 3 7.32387 3.94821 5.63604 5.63604C3.94821 7.32387 3 9.61305 3 12Z"
+                stroke={colors.buttons.utility}
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
         </svg>
     );
 }
@@ -70,7 +75,7 @@ function SubmitSpot({ current_callsign }) {
                     Submit new spot
                 </h3>
             }
-            button={<SubmitIcon size="40"></SubmitIcon>}
+            button={<SubmitIcon size="32"></SubmitIcon>}
             on_open={() => reset_temp_data()}
         >
             <table
